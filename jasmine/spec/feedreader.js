656e2f1e365f48f96256a075ada282236f83ead7
Amanda Solis <aysolis14@gmail.com>
2018-07-20 10:08:05 -0500

    Updated jasmine files to 3.1.0

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -46,6 +46,9 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "The menu" */
+    describe('The menu', () => {
+        
+    })
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
