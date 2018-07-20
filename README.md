ec959affe8335ecf0cf6564f57645275ccf2f49c
Amanda Solis <aysolis14@gmail.com>
2018-07-20 11:10:37 -0500

    Finalization of tests and README

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# feed-reader
+# Feed-Reader Project
+This project involves writing test suites with Jasmine for a web application that reads RSS feed.
+
+## Test Functions
+To begin, first download the zip folder to run locally on your computer, or clone the repository. Click the green "Clone or Download" button. All files inside the respository are REQUIRED. 
+
+- The menu 
+    - the element is hidden by default
+    - visibility is changed when clicked
+- Initial Entries
+    - has at least a single .entry element
+- New Feed Selection
+    - content is changed
+- RSS Feeds
+    - URL is defined and not empty
+    - name is defined and not empty
+    - are defined
+
+## Dependencies 
+- Jasmine 3.1.0
+- Google Fonts Roboto
--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -45,36 +45,49 @@ $(function() {
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
+    /*test to view the menu and ensure it is hidden by default and will hide/show when clicked*/
     describe('The menu', () => {
+        let m = document.querySelector('body');
+        it('the element is hidden by default', () => {
+            expect(m).toHaveClass('menu-hidden');
+        })
+
+        it('visibility is changed when clicked', () => {
+            document.querySelector('.menu-icon-link').click();
+            expect(m).not.toHaveClass('menu-hidden');
+            document.querySelector('.menu-icon-link').click();
+            expect(m).toHaveClass('menu-hidden');
+        })
+    });
+
+    /* test to look at initial entries and ensure there is at least a single element*/
+        describe('Initial Entries', () => {
+            beforeEach((done) => {
+                loadFeed(0, done);
+            });
+
+            it('has at least a single .entry element', () => {
+                let a = document.querySelector('.feed');
+                expect(a.childElementCount).toBeGreaterThan(0);
+            })
+        });
         
-    })
-
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
-
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
-
-    /* TODO: Write a new test suite named "Initial Entries" */
-
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
 
     /* TODO: Write a new test suite named "New Feed Selection" */
-
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
+        describe('New Feed Selection', () => {
+            let newFeedA, newFeedB;
+            beforeEach((done) => {
+                loadFeed(0, () => {
+                    newFeedA = document.querySelector('.feed').innerHTML;
+                    loadFeed(1, () => {
+                        newFeedB = document.querySelector('.feed').innerHTML;
+                        done();
+                    })
+                })
+            })
+            it ('content is changed', () => {
+                expect(newFeedA).not.toBe(newFeedB);
+            })
+        });
+        
 }());
